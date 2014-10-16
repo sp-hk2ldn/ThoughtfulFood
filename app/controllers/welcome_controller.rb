bdbb70d61fc77e249bcb87ce248083315dbcb4ca
Stephen <stephen.parker@timehascome.org>
2014-10-16 17:24:36 +0800

    made sure was in working state

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+  	# @items = FoodItem.all
  	@delivery = Time.now + (2700)		
 		if params[:admin] == "true"
 			render 'index'
 		else
 			render 'under_construction'
+
 	end
 	end
 end
