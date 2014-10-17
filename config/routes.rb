3f252696b91ca830e21f20c4709cd7eb5b7795d1
Stephen <stephen.parker@timehascome.org>
2014-10-17 15:52:38 +0800

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'food_item/show/:id' => 'food_item#show'
+
+  # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
