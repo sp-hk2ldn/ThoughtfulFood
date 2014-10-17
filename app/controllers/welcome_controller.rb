1270892e6607de39af7bbb9096f46cc8a7d3b938
Stephen <stephen.parker@timehascome.org>
2014-10-17 15:31:02 +0800

    changed image

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-  	@item = FoodItem.find_by_name("Crab Yellow Curry")
+  	@item1 = FoodItem.where(cuisine: "Western").sample
+  	@item2 = FoodItem.where(cuisine: "Adventurous").sample
+  	@item3 = FoodItem.where(cuisine: "Asian").sample
  	@delivery = Time.now + (2700)		
 		if params[:admin] == "true"
 			render 'index'
